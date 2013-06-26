f2fe49a597da6e3c560ae2c59a7183c9a3a759cf
Rails <rails@Railss-MacBook-Air.local>
2013-06-26 14:04:06 +0100

    3qfev

--- app/models/user.rb
+++ app/models/user.rb
@@ -60,33 +60,33 @@ class User < ActiveRecord::Base
 
   private
 
-  def add_user_to_mailchimp
-    return if email.include?(ENV['ADMIN_EMAIL'])
-    mailchimp = Gibbon.new
-    result = mailchimp.list_subscribe({
-      :id => ENV['MAILCHIMP_LIST_ID'],
-      :email_address => self.email,
-      :double_optin => false,
-      :update_existing => true,
-      :send_welcome => true
-      })
-    Rails.logger.info("Subscribed #{self.email} to MailChimp") if result
-  rescue Gibbon::MailChimpError => e
-    Rails.logger.info("MailChimp subscribe failed for #{self.email}: " + e.message)
-  end
+  #def add_user_to_mailchimp
+  #  return if email.include?(ENV['ADMIN_EMAIL'])
+  #  mailchimp = Gibbon.new
+  #  result = mailchimp.list_subscribe({
+  #    :id => ENV['MAILCHIMP_LIST_ID'],
+  #    :email_address => self.email,
+  #    :double_optin => false,
+  #    :update_existing => true,
+  #    :send_welcome => true
+  #    })
+  #  Rails.logger.info("Subscribed #{self.email} to MailChimp") if result
+  #rescue Gibbon::MailChimpError => e
+  #  Rails.logger.info("MailChimp subscribe failed for #{self.email}: " + e.message)
+  #end
 
-  def remove_user_from_mailchimp
-    mailchimp = Gibbon.new
-    result = mailchimp.list_unsubscribe({
-      :id => ENV['MAILCHIMP_LIST_ID'],
-      :email_address => self.email,
-      :delete_member => true,
-      :send_goodbye => false,
-      :send_notify => true
-      })
-    Rails.logger.info("Unsubscribed #{self.email} from MailChimp") if result
-  rescue Gibbon::MailChimpError => e
-    Rails.logger.info("MailChimp unsubscribe failed for #{self.email}: " + e.message)
-  end
+  #def remove_user_from_mailchimp
+  #  mailchimp = Gibbon.new
+  #  result = mailchimp.list_unsubscribe({
+  #    :id => ENV['MAILCHIMP_LIST_ID'],
+  #    :email_address => self.email,
+  #    :delete_member => true,
+  #    :send_goodbye => false,
+  #    :send_notify => true
+  #    })
+  #  Rails.logger.info("Unsubscribed #{self.email} from MailChimp") if result
+  #rescue Gibbon::MailChimpError => e
+  #  Rails.logger.info("MailChimp unsubscribe failed for #{self.email}: " + e.message)
+  #end
 
 end
