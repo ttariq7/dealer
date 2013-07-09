b2913718dac84324c13b2ce9bd3b2cd83c5d3149
Rails <rails@Railss-MacBook-Air.local>
2013-07-09 11:36:16 +0100

    3gers

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
 	def index
 
 
-		@user = User.find_by_id(2)
+		@user = User.find_by_id(5)
 		@vehicles = @user.vehicles
     	@vehicle = current_user.vehicles.build if signed_in?
 
