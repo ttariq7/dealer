42287a5735e0d3abab09d901dc1c540f0baa41dc
Rails <rails@Railss-MacBook-Air.local>
2013-07-11 12:01:07 +0100

    w4grev

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 gem 'rails', '3.2.13'
 gem "carrierwave"
 gem 'pg'
+gem "fog", "~> 1.3.1"
 gem "heroku"
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -11,15 +11,19 @@ class ImageUploader < CarrierWave::Uploader::Base
   # include Sprockets::Helpers::IsolatedHelper
 
   # Choose what kind of storage to use for this uploader:
-  storage :file
-  # storage :fog
+  # Choose what kind of storage to use for this uploader:
+
+  storage :fog
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
-
+  
+  def cache_dir
+  "#{Rails.root}/tmp/uploads"
+  end
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
   #   # For Rails 3.1+ asset pipeline compatibility:
