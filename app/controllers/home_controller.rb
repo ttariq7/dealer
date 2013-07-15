a508f8080ca7b0d49ae10de914ec7975f4305fa2
1NE <Serenity@1NEs-MacBook-Pro.local>
2013-07-16 02:00:40 +0400

    4rwfv

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,7 @@ before_filter :check_for_mobile
 	def index
 
 
-		@user = User.find_by_id(5)
+		@user = User.find_by_id(1)
 		@vehicles = @user.vehicles.all.sort_by { |a| -(a.price.to_i) }
     	@vehicle = current_user.vehicles.build if signed_in?
     	@micropost = Micropost.new
